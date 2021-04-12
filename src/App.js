edca75a6f952f773a66152314b684e41fa1589c5
Jonathan Ross <jross@taeltech.com>
2021-04-12 18:37:53 +0800

    implement MVP and search bar and disable multi-like

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-/* 
+/*
   Start here and work your way down the nested components.
   Not all files in the project need code added.
   Look at each file to see what props need to be passed!
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
+import Posts from './components/Posts/Posts.js';
+import SearchBar from './components/SearchBar/SearchBar.js';
+import dummyData from './dummy-data.js';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
+  const [posts, setPosts] = useState(dummyData);
+  const allPosts = dummyData;
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [searchTerm, setSearchTerm] = useState('');
 
-  const likePost = postId => {
+  const filterComments = (comments, term) => {
+    return comments.filter((comment) => comment.text.includes(term)).length > 0;
+  };
+
+  const filterPostByTerm = (post, term) => {
+    console.log(post, term);
+    return post.username.includes(term) || filterComments(post.comments, term);
+  };
+
+  const handleItemInput = (event) => {
+    const { value } = event.target;
+    setSearchTerm(value);
+    const newPosts = allPosts.filter((post) =>
+      filterPostByTerm(post, searchTerm),
+    );
+    console.log(newPosts);
+    setPosts(newPosts);
+  };
+
+  const likePost = (postId) => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -27,12 +52,23 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    setPosts(
+      posts.map((post) => {
+        if (post.id === postId) {
+          return { ...post, likes: post.likes + 1 };
+        } else {
+          return post;
+        }
+      }),
+    );
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      <SearchBar handleItemInput={handleItemInput} searchTerm={searchTerm} />
+      <Posts likePost={likePost} posts={posts} />
     </div>
   );
 };
